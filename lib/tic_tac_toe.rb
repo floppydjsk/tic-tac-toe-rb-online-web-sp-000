275da232223a4c202cc789d32b4cdd865b13086a
Daniel Kim <daniel.kim52@gmail.com>
2019-10-30 16:32:54 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,109 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+# Define your play method below
+
+def play(board)
+  turn_count = 0
+  while turn_count < 9
+    turn(board)
+    turn_count += 1
+  end
+end
+
+#current player
+def turn_count (board)
+  counter = 0
+  board.each do |turn|
+    if turn == "X" || turn == "O"
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+
+# Helper Method
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2]
+  [3,4,5]
+  [6,7,8]
+  [0,3,6]
+  [1,4,7]
+  [2,5,8]
+  [0,4,8]
+  [2,4,6]
+]
+
+#won
+def won? (board)
+  WIN_COMBINATIONS.find do |win|
+    position_taken?(board,win)
+  end
+end
+
+#full
+def full?(board)
+  board.all? do |taken|
+    taken == "X" || taken =="O"
+  end
+end
+
+#draw
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+#over
+def over?(board)
+  won?(board) || full?(board)
+end
+
+#winner
+def winner(board)
+  if winner = won?(board)
+    board[winner.first]
+  end
+end
